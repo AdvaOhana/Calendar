33d2c9a86df7b8a538f4892dd716709a5129b016
advaohana <adva1230@gmail.com>
2025-12-16 22:45:48 +0200

    Add routing and query client setup to client app
    Integrates React Router for navigation and react-query for data fetching by adding their dependencies and initializing both in the application entry point. Lays out basic route structure for login, registration, calendar, and event editing, preparing the app for future feature development.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
+const queryClient = new QueryClient()
+
 
 export default function App() {
   return (
-    <div>
-      hello
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>login page</div>} />
+          <Route path="register" element={<div>register page</div>} />
+
+          <Route element={<div>protected route</div>}>
+            <Route element={<div>applayout</div>}>
+              <Route index element={<Navigate replace to="calendar" />} />
+              <Route path="calendar" element={<div>calendar</div>} />
+              <Route path="events/:id" element={<div>edit event</div>} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Navigate replace to="login" />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
+
+
+
+
+
+
 
